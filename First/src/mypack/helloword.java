161f26958d1973bb41ebf4796b44bd868fa73ac6
HP <HP@172.16.3.239>
2022-04-25 16:03:35 +0530

    Second commit

---
+++ First/src/mypack/helloword.java
@@ -0,0 +1,24 @@
+package mypack;
+
+public class helloword {
+
+	int yearofpurchase;
+	String model;
+	double cost;
+	public void disp1()
+	{
+		System.out.println("Year of purchase"+2001);
+		
+	}
+	
+	public void disp2()
+	{
+		System.out.println("Model"+1999);
+		System.out.println("Cost"+200000);
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ First/src/mypack/thisprogram.java
@@ -0,0 +1,16 @@
+package mypack;
+
+public class thisprogram {
+
+	int age;
+	thisprogram(int age)
+	{
+		this.age= age;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+thisprogram ob = new thisprogram(8);
+System.out.println("age is "+ob.age);
+	}
+
+}
