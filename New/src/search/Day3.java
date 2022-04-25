a526953a693e148d988f8dbe5189c09f35de82c7
HP <HP@172.16.3.239>
2022-04-25 16:12:33 +0530

    First commit

---
+++ New/src/search/Day3.java
@@ -0,0 +1,24 @@
+package search;
+public class Day3 {
+	int year_ofpurchase;
+	String make;
+	int model;
+	double cost;
+	public void display1()
+	{
+		System.out.println("year of purchase"+2001);
+		System.out.println("make"+"suzuki");
+	}
+	public void display2()
+	{
+		System.out.println("model"+1999);
+		System.out.println("cost"+20000);
+		
+	}
+	public static void main(String[] args)
+	{
+		Day3 ob1 = new Day3();
+		ob1.display1();
+		ob1.display2();
+	}
+}
