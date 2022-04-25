c0a9ac610a44016af7307682f99cf9a592594c21
HP <HP@LAPTOP-KUVAU00N>
2022-04-25 19:21:08 +0530

    Second Commit

---
+++ corejava/src/myproject/helo.java
@@ -0,0 +1,16 @@
+package myproject;
+
+public class helo {
+static int age;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		helo m=new helo();
+		helo n=new helo();
+		m.age=30;
+		n.age=60;
+		System.out.println("age is"+m.age);
+		System.out.println("age is"+n.age);
+	}
+
+}
