1bbc15c838287d84f7362b780a938cb0142115ff
HP <HP@172.16.3.239>
2022-04-25 15:55:52 +0530

    Fifth commit
    Change-Id: I0d137c572abdf2b8a245695a93096d81f3ed5e9d
    Signed-off-by: HP <HP@172.16.3.239>

---
+++ corejava/src/myproject/myproject.java
@@ -0,0 +1,29 @@
+package myproject;
+
+public class myproject {
+	
+	int yearofpurchase;
+	String model;
+	double cost;
+	
+	public static void displ1()
+	{
+		System.out.println("year of purchasee"+2002);
+		System.out.println("Make:-"+"maruti");
+	}
+	public static void displ2() {
+		System.out.println("Model:-"+1999);
+		System.out.println("cost:-"+500000);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		myproject car=new myproject();
+	    car.displ1();
+	    car.displ2();
+	    
+		
+	}
+
+}
