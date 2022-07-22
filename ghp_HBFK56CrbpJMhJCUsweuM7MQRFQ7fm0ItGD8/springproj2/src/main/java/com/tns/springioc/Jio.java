dc8f6aca55998f3e9381896e32ba650084ad8e2e
HP <HP@LAPTOP-KUVAU00N>
2022-07-22 23:39:24 +0530

    Spring completed

---
+++ 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/springproj2/src/main/java/com/tns/springioc/Jio.java
@@ -0,0 +1,16 @@
+package com.tns.springioc;
+
+public class Jio implements Sim{
+
+	public void calling() {
+		// TODO Auto-generated method stub
+		System.out.println("Jio sim for Calling");
+	}
+
+	public void data() {
+		// TODO Auto-generated method stub
+		System.out.println("Jio sim for Data");
+		
+	}
+
+}
